eff80050c213fc401ebd9f79ca7cc1ef3da99aa7
delriol <101754321+delriol@users.noreply.github.com>
2024-05-07 19:35:09 -0300

    Add files via upload

---
+++ primer-parcial-solid-transporte/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace primer_parcial_solid_transporte
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<ITransporte> transportes = new List<ITransporte>();
+            TransporteRepository transporteRepository = new TransporteRepository();
+
+            Avion avion1 = new Avion("AVION 01", 0, 100, 0, 000);
+            transportes.Add(avion1);
+
+            Avion avion2 = new Avion("AVION 02", 0, 100, 0, 100);
+            transportes.Add(avion2);
+
+            Tren tren1 = new Tren("TREN 01", 0, 100);
+            transportes.Add(tren1);
+
+            Console.WriteLine("Inicio del viaje :)");
+
+            //Permite guardar el estado actual del transporte en una base de datos
+            foreach (ITransporte transporte in transportes)
+            {
+                transporteRepository.guardar(transporte);
+            }
+
+            Console.WriteLine("Fin del viaje :)");
+        }
+    }
+}
